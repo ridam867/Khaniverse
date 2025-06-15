f497430eb7c12d9d2c63d749e6c8fe3235b39d34
ridam867 <ridamgupta99@gmail.com>
2025-06-15 11:21:18 +0530

    Create Page.js

---
+++ app/Page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1 style={{ fontSize: "30px", color: "#333" }}>
+        Welcome to Khaniverse 🚀
+      </h1>
+      <p style={{ fontSize: "18px", color: "#555" }}>
+        Enter your story prompt below and watch the magic happen!
+      </p>
+    </div>
+  );
+}
